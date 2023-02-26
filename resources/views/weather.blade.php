beef25436b2351229a940a12a417914d59ed1d74
candrews6 <charlie.andrews6@mail.com>
2023-02-26 22:17:20 +0000

    add icon

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -89,7 +89,9 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                                    <img src="http://openweathermap.org/img/wn/{{ $currentWeather['weather'][0]['icon'] }}@4x.png" alt="icon">
+                                </h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
